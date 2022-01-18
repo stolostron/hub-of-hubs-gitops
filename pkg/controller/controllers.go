db6ed3aaefc1bc6c4b599349671eb3ba903478a0
maroon <maroon.ayoub@ibm.com>
2022-01-18 15:04:43 +0200

    mod versions

--- pkg/controller/controllers.go
+++ pkg/controller/controllers.go
@@ -6,6 +6,7 @@ import (
 	clustersv1 "github.com/open-cluster-management/api/cluster/v1"
 	hubv1 "github.com/open-cluster-management/hub-of-hubs-data-types/apis/config/v1"
 	"github.com/stolostron/hub-of-hubs-hub-lifecycle-management/pkg/controller/hub"
+	"github.com/stolostron/hub-of-hubs-hub-lifecycle-management/pkg/controller/kafka"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
@@ -34,7 +35,7 @@ func AddToScheme(runtimeScheme *runtime.Scheme) error {
 // AddControllers adds all the controllers to the Manager.
 func AddControllers(mgr ctrl.Manager) error {
 	addControllerFunctions := []func(ctrl.Manager) error{
-		hub.AddHubController,
+		hub.AddHubController, kafka.AddKafkaUserController,
 	}
 
 	for _, addControllerFunction := range addControllerFunctions {
